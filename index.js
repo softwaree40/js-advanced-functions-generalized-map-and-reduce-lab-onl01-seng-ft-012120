da759211c9c60c35b0a971661dcc09f2ce28059c
github username <github email address>
2020-05-14 15:00:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your functions here
+
+
+function map(oldArray,callback){
+    
+   let newArray = []
+ for(let i = 0; i < oldArray.length; i++){
+    
+ newArray.push(callback(oldArray[i]));
+
+}
+ return newArray
+
+}
+
+function reduce(sourceArray, callback,startpoint){
+    let acc 
+    let i
+    if (startpoint === undefined){
+       acc = sourceArray[0]
+     i = 1
+    } else { 
+        i = 0
+      acc = startpoint 
+}
+
+for (; i< sourceArray.length; i++){
+    
+   acc = callback(sourceArray[i],acc);
+}
+  return acc
+ }
+ 
+// function reduce(sourceArray, callback,startingPoint){
+//     let acc = startingPoint
+// for (let i = 0; i< sourceArray.length; i++){
+    
+//    acc = callback(sourceArray[i],acc);
+// }
+//   return acc
+//  }
